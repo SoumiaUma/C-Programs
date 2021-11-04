2c3e462a55af15835d56235bc8b2f46571d85c8b
Soumia Umaputhiran <93685309+SoumiaUma@users.noreply.github.com>
2021-11-03 23:26:37 -0400

    Update Calendar.cpp

--- Calendar.cpp
+++ Calendar.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int dayNumber(int day, int month, int year) 
 { 
 	//Array that stores the position that array starts on for each month
-    static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 }; 
+	static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 }; 
 	//Formula that calculates the day the month starts on for that year
-    year -= month < 3; 
-    return ( year + year/4 - year/100 + year/400 + t[month-1] + day) % 7; 
+	year -= month < 3; 
+	return ( year + year/4 - year/100 + year/400 + t[month-1] + day) % 7; 
 } 
 //Function to print calendar for one month on a specific year 
 void printMonth(int month, int year)
