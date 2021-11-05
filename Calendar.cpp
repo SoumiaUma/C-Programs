2f79995025023292378a72b40f209429446e9163
Soumia Umaputhiran <93685309+SoumiaUma@users.noreply.github.com>
2021-11-05 12:07:54 -0400

    Update Calendar.cpp

--- Calendar.cpp
+++ Calendar.cpp
@@ -1,221 +1,109 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
-//Function to determine what day month starts on 
-int dayNumber(int day, int month, int year) 
-{ 
-	//Array that stores the position that array starts on for each month
-	static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 }; 
-	//Formula that calculates the day the month starts on for that year
-	year -= month < 3; 
-	return ( year + year/4 - year/100 + year/400 + t[month-1] + day) % 7; 
-} 
-//Function to print calendar for one month on a specific year 
-void printMonth(int month, int year)
-{
-	//Array to store the month name 
-	string monthName[12];
-	//Array to number of days in a month
-	int day;
-	//If else statements to store the number of days in the month entered and the name of the month
-	if (month == 1)
-	{
-		day=31;
-		monthName[0]= "January";
-	}
-	//Checking to see if year is a leap year for the month of Feburary
-	else if (month== 2)
-	{
-		monthName[1]= "Feburary";
-		// If the year is a leap year then February has 29 days 
-		if ((year % 400 == 0 || year % 4 == 0 && year % 100 != 0)) 
-        {
-			day == 29;
-		}
-		//Otherwise, Feburary has 28 days
-		else
-		{
-			day == 28; 
-		}
-	}
-	else if (month==3)
-	{
-		day=31;
-		monthName[2]= "March";
-	}
-	else if (month ==4)
-	{
-		day=30;
-		monthName[3]= "April";
-	}
-	else if (month ==5)
-	{
-		day=31;
-		monthName[4]= "May";
-	}
-	else if (month ==6)
-	{
-		day=30;
-		monthName[5]= "June";
-	}
-	else if (month ==7)
-	{
-		day=31;
-		monthName[6]= "July";
-	}
-	else if (month ==8)
-	{
-		day=31;
-		monthName[7]= "August";
-	}
-	else if (month ==9)
-	{
-		day=30;
-		monthName[8]= "September";
-	}
-	else if (month ==10)
-	{
-		day=31;
-		monthName[9]= "October";
-	}
-	else if (month ==11)
-	{
-		day=30;
-		monthName[10]= "November";
-	}
-	else if (month == 12)
-	{
-		day=31;
-		monthName[11]= "December";
-	}
-	//Printing heading for the calendar
-	cout << "Calendar - " << year << endl;
-	cout << "Month: " << monthName[month-1] << endl;
-	//Creating a variable to store the current day the month will start with using the dayNumber function
-	int current = dayNumber (1, 1, year); 
-	//Setting up the days in the calendar 
-	cout << "Sun  Mon  Tue  Wed  Thu  Fri  Sat" << endl;
-	int col;
-	//Creating empty spaces for days with nothing at the beggining of the month
-	for (col = 0; col < current; col++)
-	{
-        cout << "     "; 
-	}
-	//Displaying the day number under each column in the calendar
-    for (int j = 1; j<= day; j++) 
-    { 
-		if (j<10)
-		{
-			cout << j << "    "; 
-		}
-		else
-		{
-			cout << j << "   ";
-		}
-		col++;
-		//Moving output to next line when col is greater than 6
-        if (col > 6) 
-        {
-			//Setting col to 0 for next row 
-            col = 0; 
-            cout << "\n"; 
-        } 
-    } 
-	//Setting the current day to the value of col
-    current = col; 
+
+//Function to calculates which day of the week, the first day of the month falls on, for a month in a year
+//Used this website to get a formula: (http://www.cplusplus.com/forum/general/174165/)
+int firstDay(int year, int month, int day) 
+{                                                  
+    int x, y, z;
+    x = (14 - month) / 12;
+    y = year - x;
+    z = month + (12 * x) - 2;
+    return (day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * z) / 12)) % 7;
 }
+
+//Function that displays yearly calendar
 void printYear(int year)
 {
-	//Array to store the month name
-	string monthName[12] = {"January", "February", "March", "April", 
-					        "May", "June", "July", "August","September", 
-					        "October", "November", "December"}; 
-	//Variable to store the amount of days in the month
-	int days;
-	//Variable to set the number of days in each month
-	int day;
-	//Creating an array to store the amount of days in a month
-	int months [12];
-	//Initializing each position in the months array to num of days in that month
-	months[0] = 31;
-	//Checking to see if year is a leap year for the month of Feburary
-	monthName[1]= "Feburary";
-	// If the year is a leap year then February has 29 days 
-	if ((year % 400 == 0 || year % 4 == 0 && year % 100 != 0)) 
-	{
-		day == 29;
-		months[1]=29;
-	}
-	//Otherwise, Feburary has 28 days
-	else
-	{
-		day == 28; 
-		months[1]=28;
-	}
-	months[2] = 31;
-	months[3]= 30;
-	months[4] = 31;
-	months [5]= 30;
-	months [6] = 31;
-	months [7] = 31;
-	months[8] = 30;
-	months [9] = 31;
-	months[10]=30;
-	months[11]=31;
-	//Creating a variable to store the current day the month will start with using the dayNumber function
-	int current = dayNumber (1, 1, year);
-	//Displaying the header for the calendar 
-	cout << "Calendar - " << year << endl;
-	for (int i = 0; i < 12; i++) 
-    { 
-		//Setting the number of days each month has 
-		days = months[i];
-		//Printing the month name 
-        cout << endl << monthName[i] << ":" << endl; 
-		//Setting the columns of the calendar with the day of the week
-		cout << "Sun  Mon  Tue  Wed  Thu  Fri  Sat" << endl;
-		int col;
-		for (col = 0; col < current; col++)
-		{
-            cout << "     "; 
-		}
-        for (int j = 1; j<= days; j++) 
-        { 
-			if (j<10)
-			{
-				cout << j << "    "; 
-			}
-			else
-			{
-				cout << j << "   ";
-			} 
-			col++;
-            if (col > 6) 
-            { 
-                col= 0; 
-                cout << "\n"; 
-            } 
-        } 
-        current = col; 
-	}
-	
+    int week=0;
+    string monthNames[]={"January", "Feburary","March","April","May","June","July","August","September","October","November","December"};
+    //Array to store number of days in each month 
+    int monthDays[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    //Check for leap year and if so, add extra day to February
+    if ((year%4==0 and year%100!=0) or (year%400==0)){ 
+        monthDays[1]=29;
+    }
+    cout<<endl;
+    //Prinitng the calendar for each month in the year 
+    for (int month=0;month<12;month++){
+        cout<<monthNames[month]<<" "<<year<<endl;
+        //Displaying the names of the days in the week
+        cout << "Sun Mon Tue Wed Thu Fri Sat" << endl;
+        //Filling days of the week without dates with blank spaces (so until first day of the month is reached) 
+        for (week=0;week<firstDay(year, month+1,1);week++)
+        {
+            cout<<"    ";
+        }
+        //Print each day of the month under the corrct day of the week
+        for(int day=1;day<=monthDays[month];day++)
+        {
+            printf("%3d ",day);
+            //Check if end of week (7 days) has passed and if so, set the week to the beginning and go next line 
+            if (++week>6)
+            {
+                cout<<endl;
+                week=0;
+            }
+        }
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+    }
 }
-int main(){
-	//Creating a variable to store user's option for which type of calender the user would like to view 
-	int calendar,month,year;
-	//Array to display the options for the month user would like to view the calendar for 
+
+//Function that displays the monthly claendar
+void printMonth(int year, int month)
+{
+    int week = 0;
+    string months[]={"January", "Feburary","March","April","May","June","July","August","September","October","November","December"};
+    int monthDays[]={31,28,31,30,31,30,31,31,30,31,30,31};//make array of days in each month
+
+    if ((year%4==0 and year%100!=0) or (year%400==0))//add extra day to Feb. if leap year
+    {
+        monthDays[1]=29;
+    }
+    //Printing heading for the calendar
+    cout << endl;
+    cout << "Calendar - " << year << endl;
+    cout << "Month: " << months[month-1] << endl;
+    cout << "Sun Mon Tue Wed Thu Fri Sat" << endl;
+    //Filling days of the week without dates with blank spaces (so until first day of the month is reached) 
+    for (week=0;week<firstDay(year, month,1);week++)
+    {
+        cout<<"    ";
+    }
+    //Print each day of month under corrct day of the week
+    for (int i=1;i<=monthDays[month-1];i++)
+    {
+        printf("%3d ",i);
+        //Check if end of week (7 days) has passed and if so, set the week to the beginning and go next line 
+        if (++week>6)
+        {
+            cout<<endl;
+            week=0;
+        }
+    }
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+}
+
+int main() {
+    int year, month, choice;
+    //Array to display the options for the month user would like to view the calendar for 
 	string monthName[12] = {"1. January", "2. February", "3. March", "4. April", 
 					   "5. May", "6. June", "7. July", "8. August","9. September", 
 					   "10. October", "11. November", "12. December"}; 
-	
-	//Getting user input for type of calendar they wouant to view 
-	cout << "Which calendar would you like to view: \n1. Month Calendar\n2. Year Calendar " << endl;
-	cin >> calendar;
-	
-	//Getting user input to user input for month calendar
-	if (calendar==1)
-	{
-		cout << "Enter the month you would like to view the calendar for:" << endl;
+    //Getting user input for type of calendar they wouant to view 
+    cout << "Which calendar would you like to view: \n1. Month Calendar\n2. Year Calendar " << endl;
+    cin >> choice;
+
+    //Display monthly calendar options and then display the calendar of the chosen month
+    if (choice==1)
+    {
+        cout << "Enter the month you would like to view the calendar for:" << endl;
 		for (int i=0;i<12;i++)
 		{
 			cout << monthName[i] << endl;
@@ -223,28 +111,23 @@ int main(){
 		cin >> month;
 		cout << "Enter the year you would like to view the calendar for: " << endl;
 		cin >> year;
-		
-		printMonth(month,year);
-		cout << endl;
-	}
-	//Getting user input for the year calendar 
-	else if (calendar==2)
-	{
-		cout << "Enter the year you would like to view the calendar for: " <<  endl;
+        printMonth(year,month);
+        
+    }
+    //Display the yearly calendar
+    else if (choice == 2)
+    {
+        cout << "Enter the year you would like to view the calendar for: " <<  endl;
 		cin >> year;
-		printYear(year);
-		cout << endl;
-	}
-	//Invalid input is not excepted for the type of calendar. User needs to enter either 1 for month calendar or 2 for year calendar 
+        printYear(year);
+    }
+    //Invalid input is not excepted for the type of calendar. User needs to enter either 1 for month calendar or 2 for year calendar 
 	else
 	{
-		while (calendar!=1 && calendar!=2)
+		while (choice!=1 && choice!=2)
 		{
 			cout << "Invalid input! Which calendar would you like to view: \n1. Month Calendar\n2. Year Calendar " << endl;
-			cin >> calendar;
+			cin >> choice;
 		}
 	}
-	
-	return 0;
 }
-	
